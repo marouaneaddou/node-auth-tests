190685c5c1b34b75354663563facd7b83cb54f6e
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2025-11-07 20:37:17 +0100

    AUT-2 Add validator middlewares

---
+++ src/middlewares/tryCatch.middleware.ts
@@ -0,0 +1,11 @@
+import { NextFunction,
+    Request, 
+    Response }          from 'express';
+
+export const tryCatch = (controller : (req: Request, res: Response, next: NextFunction ) => Promise<unknown>)  => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        await controller(req, res, next);
+    } catch (err) {
+        next(err);
+    }
+};
---
+++ src/middlewares/validator.middlware.ts
@@ -0,0 +1,79 @@
+
+import { NextFunction,
+    Request,
+    Response
+}                       from "express";
+import { ZodError, 
+    ZodSchema }         from 'zod';
+import { StatusCodes }  from 'http-status-codes';
+
+ export function validatorBody( schema : ZodSchema ) {
+    return ( req : Request, res : Response, next : NextFunction) => {
+        try {
+            
+            console.error( 'test', req.body );
+            schema.parse( req.body );
+            next();
+        }
+        catch ( error ) {
+            if (error instanceof ZodError) {
+                res.status(StatusCodes.BAD_REQUEST).json({
+                    success : false,
+                    errors  : error.flatten(),
+                    error   : 'Invalid data',
+                });
+
+            }
+            else 
+                res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
+
+        }
+    };
+};
+
+
+export function validatorParam( schema : ZodSchema ) {
+    return ( req : Request, res : Response, next : NextFunction) => {
+        try {
+            console.error( req.params );
+            schema.parse( req.params );
+            next();
+        }
+        catch ( error ) {
+            if (error instanceof ZodError) {
+                res.status(StatusCodes.BAD_REQUEST).json({
+                    success : false,
+                    errors  : error.flatten(),
+                    error   : 'Invalid data',
+                });
+
+            }
+            else 
+                res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
+
+        }
+    };
+};
+
+export function validatorQuery( schema : ZodSchema ) {
+    return ( req : Request, res : Response, next : NextFunction) => {
+        try {
+            console.error( req.query );
+            schema.parse( req.query );
+            next();
+        }
+        catch ( error ) {
+            if (error instanceof ZodError) {
+                res.status(StatusCodes.BAD_REQUEST).json({
+                    success : false,
+                    errors  : error.flatten(),
+                    error   : 'Invalid data',
+                });
+
+            }
+            else 
+                res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
+
+        }
+    };
+};
