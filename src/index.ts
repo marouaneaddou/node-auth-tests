f024719d951f07542ad776b55fbf1301aad36ab7
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2026-08-08 17:07:57 +0100

    Include Prisma migrations in application artifact

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,23 @@ import { setupSwageer } from './docs/setup';
 
 const PORT = config.port;
 
-app.listen( PORT, async () => {
-    await connectDatabase();
-    console.log(`Server is running at http://localhost:${PORT}`);
-    setupSwageer(  );
-    console.log( `Swagger is running at http://localhost:${PORT}/api/docs`);
-});
+const startServer = async ( ) => {
+    try {
+        await connectDatabase();
+        if (process.env.NODE_ENV !== "production") {
+            setupSwageer();
+        }
+    
+        app.listen(PORT, () => {
+          console.log(`Server is running at http://localhost:${PORT}`);
+          if (process.env.NODE_ENV !== "production") {
+            console.log(`Swagger is running at http://localhost:${PORT}/api/docs`);
+          }
+        });
+      } catch (error) {
+        console.error(error);
+        process.exit(1);
+      }
+}
+
+startServer();
